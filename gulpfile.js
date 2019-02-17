e349506605eb034e43ea0769034ddc4b8dff432e
Taburin Aleksey <alecsei.ich@gmail.com>
2019-02-17 18:59:46 +0300

    setup base build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 
 const WebBuilder = require('@kseirin/web-builder');
 const CopyConfig = require('@kseirin/web-builder/lib/models/CopyConfig.js');
+const HtmlConfig = require('@kseirin/web-builder/lib/models/HtmlConfig.js');
+const CssConfig = require('@kseirin/web-builder/lib/models/CssConfig.js');
+const JsConfig = require('@kseirin/web-builder/lib/models/JsConfig.js');
 
 const copy = new CopyConfig({
   src: [`src/images/**/*`],
@@ -12,6 +15,42 @@ const copy = new CopyConfig({
   clean: [`dist/images/**/*`]
 });
 
+const html = new HtmlConfig({
+  src: [`src/html/**/*`],
+  dist: {
+    'src/html': 'dist'
+  },
+  watch: [`src/html/**/*`],
+  clean: [`dist/**/*.html`]
+});
+
+const css = new CssConfig({
+  src: [`src/css/main.scss`],
+  dist: {
+    'src/css': 'dist/styles'
+  },
+  watch: [`src/css/**/*`],
+  clean: [`dist/styles/**/*`],
+  options: {
+    compiler: 'SASS'
+  }
+});
+
+const js = new JsConfig({
+  src: [`src/js/app.js`],
+  dist: {
+    'src/js': 'dist/js'
+  },
+  watch: [`src/js/**/*`],
+  clean: [`dist/js/**/*`],
+  options: {
+    compiler: 'SASS'
+  }
+});
+
 new WebBuilder({
-  copy
+  copy,
+  html,
+  css,
+  js
 });
